3f47d1eec5a2673edf07723db617023c12748abf
Robin Tram <robin.tram@gmail.com>
2018-12-12 14:11:36 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,38 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    const username = this.state.username
+    const password = this.state.password
+    username.length > 0 && password.length > 0 ? this.props.onSubmit({username, password}) : null
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit} >
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.onChange} value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.onChange} value={this.state.password} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,23 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      input: ""
+    };
+  }
+
+  change = (event) => {
+    this.setState({
+      input: event.target.value
+    }, console.log(event.target.value))
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" onChange={this.change} value={this.state.input}/>
+        <h3>Characters left: {this.props.maxChars - this.state.input.length} </h3>
       </div>
     );
   }
